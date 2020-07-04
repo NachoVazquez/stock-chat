3dc58ef9c3310cc0c4fe6ad597aaa3621668e128
Nacho Vazquez <nachovc1410@gmail.com>
2020-07-04 16:48:04 -0300

    add multidimensional dependency restrictions and fix lint issues

--- apps/stock-chat/src/app/app.component.spec.ts
+++ apps/stock-chat/src/app/app.component.spec.ts
@@ -1,11 +1,14 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
-    }).compileComponents();
+    })
+      .compileComponents()
+      .catch();
   }));
 
   it('should create the app', () => {
